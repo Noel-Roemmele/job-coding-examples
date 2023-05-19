3fcdd18d2201632f4950103535531026bd0439d6
Noel-Roemmele <94943596+Noel-Roemmele@users.noreply.github.com>
2023-05-19 15:25:12 -0600

    Delete romans.cpp

--- romans.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main() {
-    double m;
-    cin >> m;
-    cout << fixed << setprecision(0) << m * (1000.0 * (5280.0 / 4854.0)) << endl;
-    return 0;
-}
